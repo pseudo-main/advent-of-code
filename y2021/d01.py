9ef13795504660e0acfdc430744f9b6598b7f087
pseudo-main <pseudo.main.git@gmail.com>
2021-12-01 11:22:34 +0100

    add y2021 d01 p2

--- y2021/d01.py
+++ y2021/d01.py
@@ -2,15 +2,31 @@ def parse_data(data):
     return [int(measurement) for measurement in data]
 
 
+def count_increasing(numbers):
+    return sum(numbers[i] < numbers[i+1] for i in range(len((numbers)) - 1))
+
+
+def moving_average(numbers, window_size=3):
+    moving_averages = []
+    for i in range(len(numbers) - window_size + 1):
+        window = numbers[i:i+window_size]
+        moving_average = sum(window) / len(window)
+        moving_averages.append(moving_average)
+    return moving_averages
+
+
 def main():
     with open("y2021/input/d01.txt") as file:
         data = file.read().splitlines()
 
     depths = parse_data(data)
-    n_increasing = sum(
-        depths[i] < depths[i+1] for i in range(len((depths)) - 1))
+    n_increasing = count_increasing(depths)
+
+    depths_moving_averages = moving_average(depths)
+    n_increasing_moving_averages = count_increasing(depths_moving_averages)
 
     print(f"Part 1: {n_increasing}")
+    print(f"Part 2: {n_increasing_moving_averages}")
 
 
 if __name__ == "__main__":
