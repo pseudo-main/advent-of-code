28631bd9bd12404f468922985eb27f8409928918
pseudo-main <pseudo.main.git@gmail.com>
2021-12-05 15:07:16 +0100

    add y2021 d05 p2

--- y2021/d05.py
+++ y2021/d05.py
@@ -9,26 +9,52 @@ def parse_data(data):
     return lines_coords
 
 
+def coords_vertical_line(x, y1, y2):
+    return [(x, y) for y in range(min(y1, y2), max(y1, y2) + 1)]
+
+
+def coords_horizontal_line(x1, x2, y):
+    return [(x, y) for x in range(min(x1, x2), max(x1, x2) + 1)]
+
+
+def coords_diagonal_line(x1, x2, y1, y2):
+    x_sign = int((x1 - x2) / -abs(x1 - x2))
+    y_sign = int((y1 - y2) / -abs(y1 - y2))
+    line_x = list(range(x1, x2 + x_sign, x_sign))
+    line_y = list(range(y1, y2 + y_sign, y_sign))
+    return zip(line_x, line_y)
+
+
 def main():
     with open("y2021/input/d05.txt") as file:
         data = file.read().splitlines()
 
     lines_coords = parse_data(data)
     positions = defaultdict(int)
+    positions_diag = defaultdict(int)
 
     for (x1, y1), (x2, y2) in lines_coords:
         if x1 == x2:
-            line = [(x1, y) for y in range(min(y1, y2), max(y1, y2) + 1)]
+            line = coords_vertical_line(x1, y1, y2)
             for c in line:
                 positions[c] += 1
         elif y1 == y2:
-            line = [(x, y1) for x in range(min(x1, x2), max(x1, x2) + 1)]
+            line = coords_horizontal_line(x1, x2, y1)
             for c in line:
                 positions[c] += 1
+        else:
+            line = coords_diagonal_line(x1, x2, y1, y2)
+            for c in line:
+                positions_diag[c] += 1
+
+    for p in positions.keys():
+        positions_diag[p] += positions[p]
 
     n_line_intersections = sum(p >= 2 for p in positions.values())
+    n_line_intersections_diag = sum(p >= 2 for p in positions_diag.values())
 
     print(f"Part 1: {n_line_intersections}")
+    print(f"Part 2: {n_line_intersections_diag}")
 
 
 if __name__ == "__main__":
