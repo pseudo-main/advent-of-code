0dcc6be344c7e99ccd1d0d625dd05de3078799ce
pseudo-main <pseudo.main.git@gmail.com>
2021-12-07 17:06:34 +0100

    add y2021 d07 p2

--- y2021/d07.py
+++ y2021/d07.py
@@ -1,8 +1,13 @@
 from statistics import median
+from sys import maxsize
 
 
 def parse_data(data):
     return [int(x) for x in data.split(",")]
+
+
+def triangle_n(n):
+    return n * (n+1) // 2
 
 
 def main():
@@ -10,9 +15,18 @@ def main():
         data = file.read()
 
     ps = parse_data(data)
-    fuel_cost = sum(abs(p - median(ps)) for p in ps)
+    fuel_cost = int(sum(abs(p - median(ps)) for p in ps))
+
+    fuel_cost_2 = maxsize
+    for x in range(min(ps), max(ps) + 1):
+        fuel_cost_2_next = sum(triangle_n(abs(p - x)) for p in ps)
+        if fuel_cost_2_next > fuel_cost_2:
+            break
+
+        fuel_cost_2 = fuel_cost_2_next
 
     print(f"Part 1: {fuel_cost}")
+    print(f"Part 2: {fuel_cost_2}")
 
 
 if __name__ == "__main__":
