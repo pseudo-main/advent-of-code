8d0ecd422c3a679022baf56fde65f9ee11874576
pseudo-main <pseudo.main.git@gmail.com>
2021-12-12 21:46:43 +0100

    add y2021 d12 p2

--- y2021/d12.py
+++ y2021/d12.py
@@ -2,25 +2,27 @@ from collections import defaultdict
 
 
 def parse_data(data):
-    caves_map = defaultdict(set)
+    caves_map = defaultdict(list)
     for line in data:
         x, y = line.split("-")
         if x != 'end' and y != 'start':
-            caves_map[x].add(y)
+            caves_map[x].append(y)
         if x != 'start' and y != 'end':
-            caves_map[y].add(x)
+            caves_map[y].append(x)
 
     return caves_map
 
 
-def traverse(caves_map, current, traversed=[]):
-    traversed_small = set(cave for cave in traversed if cave.islower())
-    caves_next = caves_map[current].difference(traversed_small)
+def traverse(caves_map, cave='start', path=[], small_twice=False):
+    caves_next = caves_map[cave]
 
     n_paths = 0
     for cave_next in caves_next:
-        if cave_next != 'end':
-            n_paths += traverse(caves_map, cave_next, traversed + [current])
+        if cave_next.islower() and cave_next in path:
+            if small_twice:
+                n_paths += traverse(caves_map, cave_next, path+[cave])
+        elif cave_next != 'end':
+            n_paths += traverse(caves_map, cave_next, path+[cave], small_twice)
         else:
             n_paths += 1
 
@@ -32,9 +34,12 @@ def main():
         data = file.read().splitlines()
 
     caves_map = parse_data(data)
-    n_paths = sum(traverse(caves_map, cave) for cave in caves_map['start'])
+
+    n_paths = traverse(caves_map)
+    n_paths_special = traverse(caves_map, small_twice=True)
 
     print(f"Part 1: {n_paths}")
+    print(f"Part 2: {n_paths_special}")
 
 
 if __name__ == "__main__":
