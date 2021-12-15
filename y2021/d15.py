e1a7af78723881e871a19255bf08a90c9fe3e704
pseudo-main <pseudo.main.git@gmail.com>
2021-12-16 00:37:36 +0100

    add y2021 d15 p2

--- y2021/d15.py
+++ y2021/d15.py
@@ -44,7 +44,27 @@ def main():
     risk_levels = parse_data(data)
     risk_min = dijkstra(risk_levels)
 
+    risk_levels_full = risk_levels.copy()
+    w, h = max(risk_levels)
+
+    for (x, y), risk in risk_levels_full.copy().items():
+        risk_new = risk
+        for i in range(1, 5):
+            c_new = (x + i * (w+1), y)
+            risk_new = max((risk_new + 1) % 10, 1)
+            risk_levels_full[c_new] = risk_new
+
+    for (x, y), risk in risk_levels_full.copy().items():
+        risk_new = risk
+        for j in range(1, 5):
+            c_new = (x, y + j * (h+1))
+            risk_new = max((risk_new + 1) % 10, 1)
+            risk_levels_full[c_new] = risk_new
+
+    risk_min_full = dijkstra(risk_levels_full)
+
     print(f"Part 1: {risk_min}")
+    print(f"Part 2: {risk_min_full}")
 
 
 if __name__ == "__main__":
