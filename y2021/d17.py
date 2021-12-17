5ac9a4ba8dae0f509f8966ca0c8e89e815d10826
pseudo-main <pseudo.main.git@gmail.com>
2021-12-17 22:50:40 +0100

    add y2021 d17 p2

--- y2021/d17.py
+++ y2021/d17.py
@@ -3,15 +3,36 @@ def parse_data(data):
     return [tuple(map(int, axis.split(".."))) for axis in axes]
 
 
+def check_trajectory(dx, dy, x_min, x_max, y_min, y_max):
+    x, y = 0, 0
+    while x <= x_max and y >= y_min:
+        if x >= x_min and y <= y_max:
+            return True
+
+        x += dx
+        y += dy
+        dx = max(0, dx - 1)
+        dy -= 1
+
+    return False
+
+
 def main():
     with open("y2021/input/d17.txt") as file:
         data = file.read()
 
-    (x__min, x_max), (y_min, y_max) = parse_data(data)
+    (x_min, x_max), (y_min, y_max) = parse_data(data)
+    max_height = y_min * (y_min + 1) // 2
 
-    max_height = (y_min * (y_min + 1)) // 2
+    min_dx = int((x_min * 2) ** 0.5)
+    n_trajectories = 0
+    for dx in range(min_dx, x_max + 1):
+        for dy in range(y_min, -y_min):
+            if check_trajectory(dx, dy, x_min, x_max, y_min, y_max):
+                n_trajectories += 1
 
     print(f"Part 1: {max_height}")
+    print(f"Part 2: {n_trajectories}")
 
 
 if __name__ == "__main__":
