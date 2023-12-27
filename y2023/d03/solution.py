4322f1b59d9cb51cb2c81b94831b0aac19a29c79
pseudo-main <pseudo.main.git@gmail.com>
2023-12-27 16:24:18 +0100

    add solution part2 y2023d03

--- y2023/d03/solution.py
+++ y2023/d03/solution.py
@@ -21,17 +21,36 @@ class SchematicSymbol:
     coordinate: Coordinate
     value: str
 
+    def gear_ratio(self, schematic_part_numbers: list["SchematicNumber"]) -> int:
+        if self.value != "*":
+            return 0
+
+        gear_part_numbers = []
+        for part_number in schematic_part_numbers:
+            if any(
+                coordinate in self.coordinate.adjacent()
+                for coordinate in part_number.coordinates
+            ):
+                gear_part_numbers.append(part_number)
+
+        if len(gear_part_numbers) == 2:
+            return gear_part_numbers[0].value * gear_part_numbers[1].value
+        else:
+            return 0
+
 
 @dataclass
 class SchematicNumber:
     coordinates: list[Coordinate]
     value: int
 
-    def is_part_number(self, schematic_symbols: list[SchematicSymbol]) -> bool:
+    def is_part_number(self, schematic_symbols: list["SchematicSymbol"]) -> bool:
         for symbol in schematic_symbols:
-            for adjacent_coordinate in symbol.coordinate.adjacent():
-                if adjacent_coordinate in self.coordinates:
-                    return True
+            if any(
+                coordinate in self.coordinates
+                for coordinate in symbol.coordinate.adjacent()
+            ):
+                return True
 
         return False
 
@@ -72,6 +91,9 @@ class Solver:
         numbers, symbols = self.read_input(path)
         part_numbers = [number for number in numbers if number.is_part_number(symbols)]
         print(f"Part 1: {sum(part_number.value for part_number in part_numbers)}")
+
+        gear_ratios = [symbol.gear_ratio(part_numbers) for symbol in symbols]
+        print(f"Part 2: {sum(gear_ratios)}")
 
 
 if __name__ == "__main__":
