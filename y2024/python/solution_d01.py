bb9e52cdf2e3b0593a017d7b74b535ff39859399
pseudo-main <pseudo.main.git@gmail.com>
2024-12-03 21:55:58 +0100

    feature: add python solution y2024 d01 p01

---
+++ y2024/python/solution_d01.py
@@ -0,0 +1,22 @@
+FILEPATH = "y2024/input/input_d01.txt"
+
+
+def read_input(filepath: str) -> (list, list):
+    location_ids_a = []
+    location_ids_b = []
+
+    with open(filepath, "r") as f:
+        for line in f:
+            a, b = line.split()
+            location_ids_a.append(int(a))
+            location_ids_b.append(int(b))
+
+    return location_ids_a, location_ids_b
+
+
+if __name__ == "__main__":
+    ids_a, ids_b = read_input(FILEPATH)
+    sorted_ids_a = sorted(ids_a)
+    sorted_ids_b = sorted(ids_b)
+    total_distance = sum(abs(a - b) for a, b in zip(sorted_ids_a, sorted_ids_b))
+    print(f"Part 1: {total_distance}")
