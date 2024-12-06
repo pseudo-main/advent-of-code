5058523dcde45d13d6c228a31c1128067107fb7a
pseudo-main <pseudo.main.git@gmail.com>
2024-12-06 16:29:52 +0100

    feature: add python solution y2024 d04 p02

--- y2024/python/solution_d04.py
+++ y2024/python/solution_d04.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 FILEPATH = "y2024/input/input_d04.txt"
 
 
@@ -9,17 +11,27 @@ if __name__ == "__main__":
             for x, c in enumerate(line)
         }
 
-    word = list("XMAS")
-    deltas = (-1, 0, 1)
-    searches = []
+    deltas_xmas = (-1, 0, 1)
+    deltas_x_mas = (-1, 1)
+    n_matches_xmas = 0
+    n_matches_x_mas = 0
     for (x, y), c in word_search.items():
-        if not c == word[0]:
-            continue
+        if c == "X":
+            for dx in deltas_xmas:
+                for dy in deltas_xmas:
+                    coordinates = [(x + dx * i, y + dy * i) for i in range(4)]
+                    search = [word_search.get((x, y), "") for x, y in coordinates]
+                    n_matches_xmas += "".join(search) == "XMAS"
 
-        for dx in deltas:
-            for dy in deltas:
-                coordinates = [(x + dx * i, y + dy * i) for i in range(len(word))]
-                searches.append([word_search.get((x, y), "") for x, y in coordinates])
+        elif c == "A":
+            n_matches = 0
+            for dx in deltas_x_mas:
+                for dy in deltas_x_mas:
+                    coordinates = [(x + dx * i, y + dy * i) for i in range(-1, 2)]
+                    search = [word_search.get((x, y), "") for x, y in coordinates]
+                    n_matches += "".join(search) == "MAS"
 
-    n_matches = sum(search == word for search in searches)
-    print(f"Part 1: {n_matches}")
+            n_matches_x_mas += n_matches == 2
+
+    print(f"Part 1: {n_matches_xmas}")
+    print(f"Part 2: {n_matches_x_mas}")
