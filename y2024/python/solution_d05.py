1e06da5109fba9e2704305db7d169de54a9214c5
pseudo-main <pseudo.main.git@gmail.com>
2024-12-06 17:11:10 +0100

    feature: add python solution y2024 d05 p01

---
+++ y2024/python/solution_d05.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+FILEPATH = "y2024/input/input_d05.txt"
+
+
+def is_in_correct_order(pages: list[int], rules: dict[int : set[int]]) -> bool:
+    return all(not rules[p].intersection(pages[i:]) for i, p in enumerate(pages))
+
+
+if __name__ == "__main__":
+    with open(FILEPATH, "r") as f:
+        part_a, part_b = (part.split("\n") for part in f.read().strip().split("\n\n"))
+
+    rules = defaultdict(set)
+    for rule in part_a:
+        x, y = map(int, rule.split("|"))
+        rules[y].add(x)
+
+    updates = [list(map(int, update.split(","))) for update in part_b]
+    correct_updates = [pages for pages in updates if is_in_correct_order(pages, rules)]
+    total = sum(update[(len(update) - 1) // 2] for update in correct_updates)
+    print(f"Part 1: {total}")
