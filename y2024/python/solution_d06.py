a13b750eec98273a93bd19ebc8c7393631dee9bf
pseudo-main <pseudo.main.git@gmail.com>
2024-12-07 10:23:00 +0100

    refactor: extract patrol function

--- y2024/python/solution_d06.py
+++ y2024/python/solution_d06.py
@@ -1,4 +1,26 @@
-FILEPATH = "y2024/input/input_d06.txt"
+FILEPATH = "y2024/samples/sample_d06_p01.txt"
+
+
+def patrol(map: dict[(int, int): str], x: int, y: int) -> set[(int, int)]:
+    dx, dy = (0, -1)
+    directions = {
+        (0, -1): (1, 0),
+        (1, 0): (0, 1),
+        (0, 1): (-1, 0),
+        (-1, 0): (0, -1),
+    }
+    visited = {(x, y)}
+
+    while (x, y) in map:
+        if (x + dx, y + dy) not in map:
+            return visited
+
+        if map[(x + dx, y + dy)] != "#":
+            x += dx
+            y += dy
+            visited.add((x, y))
+        else:
+            dx, dy = directions[(dx, dy)]
 
 
 if __name__ == "__main__":
@@ -10,24 +32,5 @@ if __name__ == "__main__":
         }
 
     x, y = list(map.keys())[list(map.values()).index("^")]
-    visited = {(x, y)}
-    dx, dy = (0, -1)
-    turns = {
-        (0, -1): (1, 0),
-        (1, 0): (0, 1),
-        (0, 1): (-1, 0),
-        (-1, 0): (0, -1),
-    }
-
-    while (x, y) in map:
-        if (x + dx, y + dy) not in map:
-            break
-
-        if map[(x + dx, y + dy)] != "#":
-            x += dx
-            y += dy
-            visited.add((x, y))
-        else:
-            dx, dy = turns[(dx, dy)]
-
+    visited = patrol(map, x, y)
     print(f"Part 1: {len(visited)}")
