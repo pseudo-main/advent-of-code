a3c9bc526c42247705201230e6fc820cba9696a9
pseudo-main <pseudo.main.git@gmail.com>
2024-12-07 08:48:37 +0100

    refactor: move linesplits out of file reading

--- y2024/python/solution_d05.py
+++ y2024/python/solution_d05.py
@@ -9,14 +9,14 @@ def is_in_correct_order(pages: list[int], rules: dict[int : set[int]]) -> bool:
 
 if __name__ == "__main__":
     with open(FILEPATH, "r") as f:
-        part_a, part_b = (part.split("\n") for part in f.read().strip().split("\n\n"))
+        part_a, part_b = f.read().strip().split("\n\n")
 
     rules = defaultdict(set)
-    for rule in part_a:
+    for rule in part_a.split():
         x, y = map(int, rule.split("|"))
         rules[x].add(y)
 
-    updates = [list(map(int, update.split(","))) for update in part_b]
+    updates = [list(map(int, update.split(","))) for update in part_b.split()]
     correct_updates = [pages for pages in updates if is_in_correct_order(pages, rules)]
     total = sum(update[(len(update) - 1) // 2] for update in correct_updates)
     print(f"Part 1: {total}")
