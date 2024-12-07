3b357bd91e333ef97676a87cf8deafaa62dac85b
pseudo-main <pseudo.main.git@gmail.com>
2024-12-07 10:38:10 +0100

    feature: add python solution y2024 d06 p02

--- y2024/python/solution_d06.py
+++ y2024/python/solution_d06.py
@@ -1,7 +1,7 @@
-FILEPATH = "y2024/samples/sample_d06_p01.txt"
+FILEPATH = "y2024/input/input_d06.txt"
 
 
-def patrol(map: dict[(int, int): str], x: int, y: int) -> set[(int, int)]:
+def patrol(map: dict[(int, int):str], x: int, y: int) -> (bool, set[(int, int)]):
     dx, dy = (0, -1)
     directions = {
         (0, -1): (1, 0),
@@ -10,10 +10,11 @@ def patrol(map: dict[(int, int): str], x: int, y: int) -> set[(int, int)]:
         (-1, 0): (0, -1),
     }
     visited = {(x, y)}
+    turns = {(x, y, dx, dy)}
 
     while (x, y) in map:
         if (x + dx, y + dy) not in map:
-            return visited
+            return False, visited
 
         if map[(x + dx, y + dy)] != "#":
             x += dx
@@ -21,6 +22,11 @@ def patrol(map: dict[(int, int): str], x: int, y: int) -> set[(int, int)]:
             visited.add((x, y))
         else:
             dx, dy = directions[(dx, dy)]
+
+            if (x, y, dx, dy) in turns:
+                return True, visited
+
+            turns.add((x, y, dx, dy))
 
 
 if __name__ == "__main__":
@@ -32,5 +38,14 @@ if __name__ == "__main__":
         }
 
     x, y = list(map.keys())[list(map.values()).index("^")]
-    visited = patrol(map, x, y)
+    _, visited = patrol(map, x, y)
     print(f"Part 1: {len(visited)}")
+
+    n_obstruction_positions = 0
+    for coordinate in visited:
+        map[coordinate] = "#"
+        is_loop, _ = patrol(map, x, y)
+        n_obstruction_positions += is_loop
+        map[coordinate] = "."
+
+    print(f"Part 2: {n_obstruction_positions}")
