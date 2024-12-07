8ec20737a6ddaac0408f9bff8b288ab9230060c4
pseudo-main <pseudo.main.git@gmail.com>
2024-12-07 08:50:43 +0100

    refactor: rename is_in_correct_order to is_correct_order

--- y2024/python/solution_d05.py
+++ y2024/python/solution_d05.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 FILEPATH = "y2024/input/input_d05.txt"
 
 
-def is_in_correct_order(pages: list[int], rules: dict[int : set[int]]) -> bool:
+def is_correct_order(pages: list[int], rules: dict[int : set[int]]) -> bool:
     return not any(rules[p].intersection(pages[:i]) for i, p in enumerate(pages))
 
 
@@ -17,6 +17,6 @@ if __name__ == "__main__":
         rules[x].add(y)
 
     updates = [list(map(int, update.split(","))) for update in part_b.split()]
-    correct_updates = [pages for pages in updates if is_in_correct_order(pages, rules)]
+    correct_updates = [pages for pages in updates if is_correct_order(pages, rules)]
     total = sum(update[(len(update) - 1) // 2] for update in correct_updates)
     print(f"Part 1: {total}")
