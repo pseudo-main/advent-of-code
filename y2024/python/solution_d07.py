1e69fec3e534d9f06b9ac6af761e64724a99c7cf
pseudo-main <pseudo.main.git@gmail.com>
2024-12-08 08:45:31 +0100

    refactor: equations as list of ints, separate calibrate function

--- y2024/python/solution_d07.py
+++ y2024/python/solution_d07.py
@@ -3,25 +3,30 @@ from itertools import product
 FILEPATH = "y2024/input/input_d07.txt"
 
 
+def calibrate(equations: list[int], operators: str) -> int:
+    calibration_result = 0
+    for result, *terms in equations:
+        operator_combos = [list(p) for p in product(operators, repeat=len(terms) - 1)]
+        if any(is_possible(result, terms, combo) for combo in operator_combos):
+            calibration_result += result
+
+    return calibration_result
+
+
 def is_possible(result: int, terms: list[int], operators: list[str]) -> bool:
     value = terms[0]
     for operator, term in zip(operators, terms[1:]):
-        value = eval(str(value) + operator + term)
+        value = eval(str(value) + operator + str(term))
 
     return value == result
 
 
 if __name__ == "__main__":
     with open(FILEPATH, "r") as f:
-        equations = {
-            int(equation.split(":")[0]): equation.split(":")[1].split()
+        equations = [
+            map(int, equation.replace(":", " ").split())
             for equation in f.read().splitlines()
-        }
+        ]
 
-    calibration_result = 0
-    for result, terms in equations.items():
-        operator_combos = [list(p) for p in product("+*", repeat=len(terms) - 1)]
-        if any(is_possible(result, terms, operators) for operators in operator_combos):
-            calibration_result += result
-
+    calibration_result = calibrate(equations, "+*")
     print(f"Part 1: {calibration_result}")
