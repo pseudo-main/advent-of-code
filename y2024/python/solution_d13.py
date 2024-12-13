774276be422dcff6a2ee55c5a0e50e68b4898bb6
pseudo-main <pseudo.main.git@gmail.com>
2024-12-13 08:00:50 +0100

    refactor: cramer's rule for efficiency

--- y2024/python/solution_d13.py
+++ y2024/python/solution_d13.py
@@ -12,19 +12,15 @@ if __name__ == "__main__":
         ]
 
     tokens_spent = 0
-    for dx_a, dy_a, dx_b, dy_b, x_p, y_p in instructions:
-        solutions = []
-
-        for i in range(101):
-            dx, dy = (x_p - dx_a * i, y_p - dy_a * i)
-            if (dx or dy) <= 0:
-                break
+    c1 = 3
+    c2 = 1
+    for v1_x, v1_y, v2_x, v2_y, v3_x, v3_y in instructions:
+        det = v1_x * v2_y - v1_y * v2_x
+        if det != 0:
+            x1 = (v3_x * v2_y - v3_y * v2_x) / det
+            x2 = (v1_x * v3_y - v1_y * v3_x) / det
             
-            j = dx // dx_b
-            if j <= 100 and (j * dy_b == dy):
-                solutions.append((i*3 + j, i, j))
-
-        if solutions:
-            tokens_spent += sorted(solutions)[0][0]
+            if (x1 % 1 == 0) and (x2 % 1 == 0) and (0 <= x1 <= 100) and (0 <= x2 <= 100):
+                tokens_spent += int(x1 * c1 + x2 * c2)
 
     print(f"Part 1: {tokens_spent}")
