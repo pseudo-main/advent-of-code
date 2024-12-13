fcad1e7d97065aa463c57efa8648cb7c27148668
pseudo-main <pseudo.main.git@gmail.com>
2024-12-13 08:19:12 +0100

    refactor: use .isinteger() for readability

--- y2024/python/solution_d13.py
+++ y2024/python/solution_d13.py
@@ -29,13 +29,18 @@ if __name__ == "__main__":
 
     for v1_x, v1_y, v2_x, v2_y, v3_x, v3_y in instructions:
         x1, x2 = cramers_rule(v1_x, v1_y, v2_x, v2_y, v3_x, v3_y)
-        if (x1 % 1 == 0) and (x2 % 1 == 0) and (0 <= x1 <= 100) and (0 <= x2 <= 100):
+        if (
+            x1.is_integer()
+            and x2.is_integer()
+            and (0 <= x1 <= 100)
+            and (0 <= x2 <= 100)
+        ):
             tokens_spent += int(x1 * c1 + x2 * c2)
 
         x1_p2, x2_p2 = cramers_rule(
             v1_x, v1_y, v2_x, v2_y, v3_x + int(1e13), v3_y + int(1e13)
         )
-        if (x1_p2 % 1 == 0) and (x2_p2 % 1 == 0):
+        if x1_p2.is_integer() and x2_p2.is_integer():
             tokens_spent_p2 += int(x1_p2 * c1 + x2_p2 * c2)
 
     print(f"Part 1: {tokens_spent}")
