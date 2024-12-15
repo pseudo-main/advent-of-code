e500f09f929f39d0151303f47adcde3322aebe97
pseudo-main <pseudo.main.git@gmail.com>
2024-12-15 11:20:54 +0100

    refactor: extract move_robots function

--- y2024/python/solution_d14.py
+++ y2024/python/solution_d14.py
@@ -3,6 +3,14 @@ import re
 FILEPATH = "y2024/input/input_d14.txt"
 
 
+def move_robots(
+    robots: list[list[int]], max_x: int, max_y: int, s: int
+) -> list[list[int]]:
+    return [
+        [(x + s * dx) % max_x, (y + s * dy) % max_y, dx, dy] for x, y, dx, dy in robots
+    ]
+
+
 if __name__ == "__main__":
     pattern = re.compile(r"-?\d+")
     with open(FILEPATH, "r") as f:
@@ -11,15 +19,12 @@ if __name__ == "__main__":
         ]
 
     max_x, max_y = 101, 103
+    robots_100s = move_robots(robots, max_x, max_y, 100)
+
     split_x = max_x // 2
     split_y = max_y // 2
-    s = 100
-
-    robots = [
-        [(x + s * dx) % max_x, (y + s * dy) % max_y, dx, dy] for x, y, dx, dy in robots
-    ]
     q_counts = [0, 0, 0, 0]
-    for x, y, _, _ in robots:
+    for x, y, _, _ in robots_100s:
         if x < split_x and y < split_y:
             q_counts[0] += 1
         elif x > split_x and y < split_y:
